4d0134522d5764fe193a7dd3c229e7e2a9a0b17d
CSM-intaniger <tanakornp@cleversecorp.com>
2023-10-28 22:25:59 +0700

    feat(handler): :safety_vest: storyline: validation logic

--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,42 @@ interface ICreateCourseDto
   duration: number;
 }
 
+const MINUTE_TO_SECONDS = 60;
+const HOUR_TO_MINUTE = 60;
+
+const toSeconds = (hours: number) => hours * MINUTE_TO_SECONDS * HOUR_TO_MINUTE;
+
+const COURSE_DURATION_LIMIT = toSeconds(8);
+
 app.post(
   "/course",
   async (req: Request<{}, unknown, ICreateCourseDto>, res) => {
-    console.log(req.body);
+    const { description, duration, name, start_time } = req.body;
+
+    // Validation logics
+    if (typeof duration !== "number")
+      return res.status(400).send(`Duration is not a number`);
+
+    if (duration > COURSE_DURATION_LIMIT)
+      return res
+        .status(400)
+        .send(`Duration is more than ${COURSE_DURATION_LIMIT}`);
+
+    if (isNaN(Date.parse(start_time)))
+      return res.status(400).send(`start_time is not in Date format`);
+
+    if (name.length === 0) return res.status(400).send(`name is empty`);
+
+    if (description.length === 0)
+      return res.status(400).send(`description is empty`);
 
     const result = await client.course.create({
-      data: req.body,
+      data: {
+        name,
+        description,
+        duration,
+        start_time: new Date(start_time),
+      },
     });
 
     return res.status(201).json(result);
