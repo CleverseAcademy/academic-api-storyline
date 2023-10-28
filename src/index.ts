8db95098343d4c6e60afa2d54aa8a8b3f5a98c50
CSM-intaniger <tanakornp@cleversecorp.com>
2023-10-28 23:01:26 +0700

    fix(course): :safety_vest: storyline: validate request

--- src/index.ts
+++ src/index.ts
@@ -100,6 +100,10 @@ app.get("/course", async (req, res) => {
 });
 
 app.get("/course/:id", async (req, res) => {
+  // UUID length
+  if (req.params.id.length != 36)
+    return res.status(400).send("ID is incorrect");
+
   const course = await client.course.findUnique({
     where: { id: req.params.id },
   });
@@ -111,6 +115,26 @@ app.patch(
   "/course/:id",
   async (req: Request<{ id: string }, unknown, IUpdateCourseDto>, res) => {
     const { description, duration, start_time } = req.body;
+
+    // UUID length
+    if (req.params.id.length != 36)
+      return res.status(400).send("ID is incorrect");
+
+    if (isNaN(Date.parse(start_time)))
+      return res.status(400).send(`start_time is not in Date format`);
+
+    const duration_in_seconds = Number(duration);
+
+    if (isNaN(duration_in_seconds))
+      return res.status(400).send(`Duration is ${duration_in_seconds}`);
+
+    if (duration_in_seconds > COURSE_DURATION_LIMIT)
+      return res
+        .status(400)
+        .send(`Duration is more than ${COURSE_DURATION_LIMIT}`);
+
+    if (description.length === 0)
+      return res.status(400).send(`description is empty`);
 
     const result = await client.course.update({
       data: {
@@ -126,6 +150,10 @@ app.patch(
 );
 
 app.delete("/course/:id", async (req, res) => {
+  // UUID length
+  if (req.params.id.length != 36)
+    return res.status(400).send("ID is incorrect");
+
   const result = await client.course.delete({
     where: { id: req.params.id },
   });
