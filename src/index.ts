1b949fbae6407d55aed0bae3debb5ea5e14e641c
CSM-intaniger <tanakornp@cleversecorp.com>
2023-10-29 01:54:28 +0700

    refactor(handler): :recycle: refactor validation logic

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,20 @@ import { ICreateTeacherDto } from "./entities/teacher.dto";
 const client = new PrismaClient();
 const app = express();
 
+const toNumber = (value: string) => {
+  const aNumber = Number(value);
+  if (isNaN(aNumber)) throw new Error(`${value} is not a number`);
+
+  return aNumber;
+};
+
+const toDate = (value: string) => {
+  if (isNaN(Date.parse(value))) throw new Error(`${value} is invalid datetime`);
+  return new Date(value);
+};
+
+const isEmptyString = (value: string) => value.length === 0;
+
 app.use(express.json());
 
 app.post(
@@ -48,29 +62,31 @@ app.post(
     const { description, duration, name, start_time } = req.body;
 
     // Validation logics
-    const duration_in_seconds = Number(duration);
-    if (isNaN(duration_in_seconds))
-      return res.status(400).send(`Duration is ${duration_in_seconds}`);
+    let duration_in_seconds: number = 0;
+    let parsed_time: Date = new Date();
+    try {
+      if (typeof duration === "number") duration_in_seconds = duration;
+      else duration_in_seconds = toNumber(duration);
 
-    if (duration_in_seconds > COURSE_DURATION_LIMIT)
-      return res
-        .status(400)
-        .send(`Duration is more than ${COURSE_DURATION_LIMIT}`);
+      if (duration_in_seconds > COURSE_DURATION_LIMIT)
+        throw new Error(
+          `Duration is larger than limit: ${COURSE_DURATION_LIMIT}`
+        );
 
-    if (isNaN(Date.parse(start_time)))
-      return res.status(400).send(`start_time is not in Date format`);
+      parsed_time = toDate(start_time);
 
-    if (name.length === 0) return res.status(400).send(`name is empty`);
-
-    if (description.length === 0)
-      return res.status(400).send(`description is empty`);
+      if (isEmptyString(name)) throw new Error(`name is empty`);
+      if (isEmptyString(description)) throw new Error(`description is empty`);
+    } catch (error: unknown) {
+      if (error instanceof Error) return res.status(400).send(error.message);
+    }
 
     const result = await client.course.create({
       data: {
         name,
         description,
         duration: duration_in_seconds,
-        start_time: new Date(start_time),
+        start_time: parsed_time,
       },
     });
 
@@ -108,26 +124,29 @@ app.patch(
     if (req.params.id.length != 36)
       return res.status(400).send("ID is incorrect");
 
-    if (isNaN(Date.parse(start_time)))
-      return res.status(400).send(`start_time is not in Date format`);
+    // Validation logics
+    let duration_in_seconds: number = 0;
+    let parsed_time: Date = new Date();
+    try {
+      if (typeof duration === "number") duration_in_seconds = duration;
+      else duration_in_seconds = toNumber(duration);
 
-    const duration_in_seconds = Number(duration);
+      if (duration_in_seconds > COURSE_DURATION_LIMIT)
+        throw new Error(
+          `Duration is larger than limit: ${COURSE_DURATION_LIMIT}`
+        );
 
-    if (isNaN(duration_in_seconds))
-      return res.status(400).send(`Duration is ${duration_in_seconds}`);
+      parsed_time = toDate(start_time);
 
-    if (duration_in_seconds > COURSE_DURATION_LIMIT)
-      return res
-        .status(400)
-        .send(`Duration is more than ${COURSE_DURATION_LIMIT}`);
-
-    if (description.length === 0)
-      return res.status(400).send(`description is empty`);
+      if (isEmptyString(description)) throw new Error(`description is empty`);
+    } catch (error: unknown) {
+      if (error instanceof Error) return res.status(400).send(error.message);
+    }
 
     const result = await client.course.update({
       data: {
         description,
-        start_time: start_time,
+        start_time: parsed_time,
         duration: duration_in_seconds,
       },
       where: { id: req.params.id },
